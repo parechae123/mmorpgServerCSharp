78261a697f1efbe6892c452940dc39b8ac1304c0
heoyoon <parechae@gmail.com>
2025-02-14 20:17:08 +0900

    feat : 프로젝트 설치 및 쓰레드 예제 작성

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Prgoram
+    {
+        static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰래드 시작");
+
+            while (_stop == false)
+            {
+
+            }
+            Console.WriteLine("쓰래드 종료");
+        }
+
+        static void MainThread()
+        {
+            Console.WriteLine("Hello Thread!");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+            _stop = true;
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기");
+            t.Wait();
+
+            Console.WriteLine("종료 성공");
+        }
+    }
+}
