be72748b5e468087f12107d3241274ccf8222c19
heoyoon <parechae@gmail.com>
2025-03-06 20:00:30 +0900

    feat : 클라이언트 소켓 연결 코드 작성

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //DMS == Domain, Name , System
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endpoint = new IPEndPoint(ipAddr, 7777);
+
+            //휴대폰 설정
+            Socket socket = new Socket(endpoint.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+            try
+            {
+                //문지기에게 입장문의
+                socket.Connect(endpoint);
+                Console.WriteLine($"Connect To {socket.RemoteEndPoint.ToString()}");
+                //보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                //나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
